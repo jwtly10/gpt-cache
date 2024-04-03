79e04b9714653dd762f1cfd6b1eb040af1439328
Josh Watley <watleyjosh@gmail.com>
2024-04-03 21:54:06 +0100

    fix: index service endpoint

--- indexing_service/app/main.py
+++ indexing_service/app/main.py
@@ -16,22 +16,22 @@ faiss = FaissEmbeddingStorage(dimension=384)
 handler = FaissHandler(embedding, faiss)
 
 
-# TODO Refactor
 @app.post("/queryIndex", response_model=QueryResponse)
 async def query_index(query: QueryRequest):
     res = handler.handle_query(query.context, query.distance_threshold)
     if res["id"] is not None:
         return QueryResponse(id=res["id"], distance=res["distance"])
     else:
-        return HTTPException(status_code=204, detail="No similar context found")
+        raise HTTPException(status_code=204, detail="No similar context found")
 
 
 @app.post("/addIndex", response_model=AddResponse)
 async def add_index(query: AddRequest):
-    res = handler.handle_add(query.id, query.context)
-    # background_tasks.add_task(handler.rebuild_index)
-
-    if res["status"] == "success":
-        return AddResponse(status="success")
-    else:
-        return HTTPException(status_code=500, detail=res["message"])
+    try:
+        res = handler.handle_add(query.id, query.context)
+        if res["status"] == "success":
+            return AddResponse(status="success")
+        else:
+            raise HTTPException(status_code=500, detail=res["message"])
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
